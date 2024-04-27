b71c9db6a1e31386d90b0a432a92f24242ea3a0c
congweiH <congwei_huang@163.com>
2024-04-27 20:59:18 +0800

    项目初始化

---
+++ check-code/src/test/java/com/congwei/checkcode/CheckCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congwei.checkcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
